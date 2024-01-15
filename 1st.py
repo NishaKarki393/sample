538e462d7f5917885a3b379f11f524710edb94c1
unknown <nishakarki393@gmail.com>
2024-01-15 22:13:16 +0530

    2nd streamlit file

---
+++ 1st.py
@@ -0,0 +1,9 @@
+import streamlit as st
+st.title('campus x')
+col1,col2=st.columns(2)
+
+with col1:
+    st.image('https://images.shiksha.com/mediadata/images/1645702564phpzkLseO.jpeg')
+
+with col2:
+    st.write("This is my campusx")
