a1bd28827237c4e1c18d1585d6dfc2025ee95ed3
unknown <nishakarki393@gmail.com>
2024-01-15 22:22:07 +0530

    Adding Sidebar

--- 1st.py
+++ 1st.py
@@ -3,7 +3,10 @@ st.title('campus x')
 col1,col2=st.columns(2)
 
 with col1:
-    st.image('https://images.shiksha.com/mediadata/images/1645702564phpzkLseO.jpeg')
+    st.image('https://images.shiksha.com/mediadata/images/1645702564phpzkLseO.jpegcl')
 
 with col2:
-    st.write("This is my campusx")
+    st.write("This is my campusx") 
+
+
+st.sidebar.write("Choose one",['1','2'])
