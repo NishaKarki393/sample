496abed991c6543637b714d72197f5ddcfd3a239
unknown <nishakarki393@gmail.com>
2024-01-16 07:22:48 +0530

    Updating the header image

--- 1st.py
+++ 1st.py
@@ -3,7 +3,7 @@ st.title('campus x')
 col1,col2=st.columns(2)
 
 with col1:
-    st.image('https://images.shiksha.com/mediadata/images/1645702564phpzkLseO.jpeg')
+    st.image('https://th.bing.com/th/id/R.6af6fd9c37f0de4abb34ea0fd20acce3?rik=55mqMmrTutVR0Q&pid=ImgRaw&r=0')
 
 with col2:
     st.write("This is my campusx")
